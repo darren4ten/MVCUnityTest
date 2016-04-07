83ce26049b66af4caa929c5e693a042831299b4f
darren4ten <darren4ten@163.com>
2016-04-07 16:24:35 +0800

    在Account的登录页面添加漂亮的验证码

---
+++ MvcAppTest/Helper/ExtendMvcHtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppTest.Helper
+{
+    public static class ExtendMvcHtml
+    {
+        public static MvcHtmlString ToolButton(this HtmlHelper helper, string id, string icon, string text, bool showSeparator)
+        {
+            StringBuilder sb = new StringBuilder(100);
+            sb.AppendFormat("<a id=\"{0}\" style=\"float: left;\" class=\"l-btn l-btn-plain\">", id);
+            sb.AppendFormat("<span class=\"l-btn-left\"><span class=\"l-btn-text {0}\" style=\"padding-left: 20px;\">", icon);
+            sb.AppendFormat("{0}</span></span></a>", text);
+            if (showSeparator)
+            {
+                sb.Append("<div class=\"datagrid-btn-separator\"></div>");
+            }
+            return new MvcHtmlString(sb.ToString());
+        }
+    }
+}
